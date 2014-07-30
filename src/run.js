bfee7d4a56963108b48f81ed74fd940e59e55b5b
James Shore <jshore@jamesshore.com>
2014-07-30 10:42:24 -0700

    Mocked up financial projector application

--- src/run.js
+++ src/run.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
 (function() {
 	"use strict";
+
 	var server = require("./server/server.js");
 
 	var port = process.argv[2];
