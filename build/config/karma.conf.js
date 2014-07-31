7cae59bc3e132186e8f521e0c86802fb1169eaaf
James Shore <jshore@jamesshore.com>
2014-07-31 13:51:50 -0700

    Got unit tests working on IE 8

--- build/config/karma.conf.js
+++ build/config/karma.conf.js
@@ -14,7 +14,11 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-	    'src/vendor/**/*.js',
+	    // vendor files must be loaded in this order to work on IE 8
+	    'src/vendor/es5-shim-4.0.1.js',
+	    'src/vendor/es5-sham-4.0.1.js',
+	    'src/vendor/react-0.11.1.js',
+
       'generated/client/**/*.js'
     ],
 
