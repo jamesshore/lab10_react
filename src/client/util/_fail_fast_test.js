dd339ed7eee3855b9b3c7ba865e9ecfab2c83f8a
James Shore <jshore@jamesshore.com>
2014-08-06 12:40:06 -0700

    Simplified failFast tests by creating generic wrap() function

--- src/client/util/_fail_fast_test.js
+++ src/client/util/_fail_fast_test.js
@@ -22,19 +22,17 @@ describe("FailFastException", function() {
 describe("FailFast", function() {
 
 	it("checks if variable is defined", function() {
+		var unlessDefined = wrap(failFast.unlessDefined);
+
 		expect(unlessDefined("foo")).to.not.throwException();
 		expect(unlessDefined(null)).to.not.throwException();
 		expect(unlessDefined(undefined)).to.throwException(/Required variable was not defined/);
 		expect(unlessDefined(undefined, "myVariable")).to.throwException(/Required variable \[myVariable\] was not defined/);
-
-		function unlessDefined(variable, variableName) {
-			return function() {
-				failFast.unlessDefined(variable, variableName);
-			};
-		}
 	});
 
 	it("checks if variable is number", function() {
+		var unlessNumber = wrap(failFast.unlessNumber);
+
 		expect(unlessNumber(0)).to.not.throwException();
 		expect(unlessNumber("foo")).to.throwException(/Expected variable to be number, but was string/);
 		expect(unlessNumber({})).to.throwException(/Expected variable to be number, but was object/);
@@ -44,37 +42,32 @@ describe("FailFast", function() {
 		expect(unlessNumber(NaN)).to.throwException(/Expected variable to be number, but was NaN/);
 
 		expect(unlessNumber("foo", "name")).to.throwException(/Expected variable \[name\] to be number, but was string/);
-
-		function unlessNumber(variable, variableName) {
-			return function() {
-				failFast.unlessNumber(variable, variableName);
-			};
-		}
 	});
 
 	it("checks if condition is true", function() {
+		var unlessTrue = wrap(failFast.unlessTrue);
+
 		expect(unlessTrue(true)).to.not.throwException();
 		expect(unlessTrue(false)).to.throwException(/Expected condition to be true/);
 		expect(unlessTrue(false, "a message")).to.throwException(/a message/);
 		expect(unlessTrue("foo")).to.throwException(/Expected condition to be true or false/);
 		expect(unlessTrue("foo", "ignored")).to.throwException(/Expected condition to be true or false/);
-
-		function unlessTrue(variable, message) {
-			return function() {
-				failFast.unlessTrue(variable, message);
-			};
-		}
 	});
 
 	it("fails when unreachable code is executed", function() {
+		var unreachable = wrap(failFast.unreachable);
+
 		expect(unreachable()).to.throwException(/Unreachable code executed/);
 		expect(unreachable("foo")).to.throwException(/foo/);
-
-		function unreachable(message) {
-			return function() {
-				failFast.unreachable(message);
-			};
-		}
 	});
+
+	function wrap(fn) {
+		return function() {
+			var outerArgs = arguments;
+			return function() {
+				fn.apply(this, outerArgs);
+			};
+		};
+	}
 
 });
