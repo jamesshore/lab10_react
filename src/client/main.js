416c9f755033aa24fb4619c889f0197cae02bdef
James Shore <jshore@jamesshore.com>
2014-08-27 12:19:12 -0700

    Minor formatting cleanup

--- src/client/main.js
+++ src/client/main.js
@@ -1,6 +1,4 @@
-/** @jsx React.DOM */
 // Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
-
 "use strict";
 
 var ApplicationUi = require("./ui/application_ui.js");
--- src/client/ui/stock_market_table.jsx
+++ src/client/ui/stock_market_table.jsx
@@ -1,6 +1,5 @@
 /** @jsx React.DOM */
 // Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
-
 "use strict";
 
 var StockMarketRow = require("./stock_market_table_row.js");
